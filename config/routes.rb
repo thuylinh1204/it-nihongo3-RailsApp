2fee84188e22d83958020390253279f6da4cc7b2
nguyenhuongvi1712 <huongvinguyen171200@gmail.com>
2021-12-10 02:23:44 +0700

    add home-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 
 Rails.application.routes.draw do
-  resources :reviews
+    root 'static_pages#home'
+    get 'static_pages/home'
+    resources :reviews
     resources :companies
-    root 'admin#index'
+    # root 'admin#index'
     resources :users
     get 'admin/index'
     get 'sessions/new'
     get 'sessions/create'
     get 'sessions/destroy'
-    
-        controller :sessions do
-            get 'login' => :new
-            post 'login' => :create
-            delete 'logout' => :destroy
-        end
+        
+    controller :sessions do
+        get 'login' => :new
+        post 'login' => :create
+        delete 'logout' => :destroy
+    end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
