125761b1222c6a76d29982e5338e8e0b5a7a8321
tuannt2000 <tuaniq2000@gmail.com>
2021-12-09 08:43:10 +0000

    add account

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+    root 'admin#index'
+  resources :users
+  get 'admin/index'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  
+    controller :sessions do
+        get 'login' => :new
+        post 'login' => :create
+        delete 'logout' => :destroy
+    end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
