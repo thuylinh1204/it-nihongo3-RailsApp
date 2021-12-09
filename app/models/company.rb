c03bc197783ce5c2ebe034a27f38c74f944e72ef
thuylinh1204 <thuylinh12042000@gmail.com>
2021-12-09 10:26:45 +0000

    add review

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,3 @@
 class Company < ApplicationRecord
+    has_many :review, -> { order "created_at DESC"}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 
 Rails.application.routes.draw do
+  resources :reviews
     resources :companies
     root 'admin#index'
     resources :users
