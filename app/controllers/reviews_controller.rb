1eeb58a9925806ba4ff394a81663887c8c582752
nguyenhuongvi1712 <huongvinguyen171200@gmail.com>
2021-12-11 03:20:31 +0700

    css review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -26,7 +26,8 @@ class ReviewsController < ApplicationController
 
     respond_to do |format|
       if @review.save
-        format.html { redirect_to @review, notice: "Review was successfully created." }
+        url = "/companies/" + @review.company_id.to_s
+        format.html { redirect_to url, notice: "Review was successfully created." }
         format.json { render :show, status: :created, location: @review }
       else
         format.html { render :new, status: :unprocessable_entity }
